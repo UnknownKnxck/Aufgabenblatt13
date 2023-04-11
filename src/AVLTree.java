ee5385dd18e4a8901f81b7f26f4a3bd9741505b4
OmaCrocs <mirko.ungerank@snets.it>
2023-04-11 16:33:06 +0200

    + AVL Tree
    TODO:
    - Change the way the tree gets printed
    - comments

--- src/AVLTree.java
+++ src/AVLTree.java
@@ -1,3 +1,83 @@
 public class AVLTree<T extends Comparable<T>> {
+    private Node<T> root;
 
+    public AVLTree() {
+        root = null;
+    }
+
+    public void insert(T value) {
+        root = insert(root, value);
+    }
+
+    private Node<T> insert(Node<T> node, T value) {
+        if (node == null) {
+            return new Node<>(value);
+        }
+
+        if (value.compareTo(node.getValue()) < 0) {
+            node.setLeft(insert(node.getLeft(), value));
+        } else if (value.compareTo(node.getValue()) > 0) {
+            node.setRight(insert(node.getRight(), value));
+        } else {
+            // value already exists in tree
+            return node;
+        }
+
+        // update height of the current node
+        node = balance(node);
+
+        return node;
+    }
+
+    private int height(Node<T> node) {
+        if (node == null) {
+            return -1;
+        }
+        return 1 + Math.max(height(node.getLeft()), height(node.getRight()));
+    }
+
+    private int getBalance(Node<T> node) {
+        if (node == null) {
+            return 0;
+        }
+        return height(node.getLeft()) - height(node.getRight());
+    }
+
+    private Node<T> rotateLeft(Node<T> node) {
+        Node<T> newRoot = node.getRight();
+        node.setRight(newRoot.getLeft());
+        newRoot.setLeft(node);
+        return newRoot;
+    }
+
+    private Node<T> rotateRight(Node<T> node) {
+        Node<T> newRoot = node.getLeft();
+        node.setLeft(newRoot.getRight());
+        newRoot.setRight(node);
+        return newRoot;
+    }
+
+    private Node<T> balance(Node<T> node) {
+        int balance = getBalance(node);
+
+        // left heavy
+        if (balance > 1) {
+            // left-right case
+            if (getBalance(node.getLeft()) < 0) {
+                node.setLeft(rotateLeft(node.getLeft()));
+            }
+            return rotateRight(node);
+        }
+        // right heavy
+        else if (balance < -1) {
+            // right-left case
+            if (getBalance(node.getRight()) > 0) {
+                node.setRight(rotateRight(node.getRight()));
+            }
+            return rotateLeft(node);
+        }
+
+        // balanced
+        return node;
+    }
 }
